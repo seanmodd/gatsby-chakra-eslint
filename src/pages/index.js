88f2b91adeab8d518a46a2c96cbe32f6084bc23e
Sean <>
2021-10-05 18:41:09 -0700

    FIXED MY STUPID ESLINT

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,23 @@
-import React from "react"
-import { Link } from "gatsby"
-import {Heading} from "@chakra-ui/react"
-import Layout from "../components/ui/layout"
-import SEO from "../components/ui/seo"
-import HeroBlock from "../components/home/HeroBlock"
-import PromotionalProducts from "../components/home/PromotionalProducts"
-import FeaturedProducts from "../components/home/FeaturedProducts"
-import MarketingButtons from "../components/home/MarketingButtons"
-import CallToAction from "../components/home/CallToAction"
+import React from 'react'
+
+import { Link } from 'gatsby'
+
+import { Heading } from '@chakra-ui/react'
+
+import Layout from '../components/ui/layout'
+import SEO from '../components/ui/seo'
+import HeroBlock from '../components/home/HeroBlock'
+import PromotionalProducts from '../components/home/PromotionalProducts'
+import FeaturedProducts from '../components/home/FeaturedProducts'
+import MarketingButtons from '../components/home/MarketingButtons'
+import CallToAction from '../components/home/CallToAction'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroBlock />
     <Heading>Heading</Heading>
-           <PromotionalProducts />
+    <PromotionalProducts />
     <FeaturedProducts />
     <MarketingButtons />
     <CallToAction />
