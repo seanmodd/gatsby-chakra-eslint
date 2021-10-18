08792d0b311115dc9270ac1a44979351fa4cc55f
Sean <>
2021-10-18 02:55:55 -0700

    REVERT TO HERE

--- src/components/_MODERN/locales/i18n.js
+++ src/components/_MODERN/locales/i18n.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-//
-import enLocales from './en.json';
-import deLocales from './de.json';
-import frLocales from './fr.json';
+import React from 'react'
 
-// ----------------------------------------------------------------------
+{
+}
+// import React from 'react';
+// import i18n from 'i18next';
+// import { initReactI18next } from 'react-i18next';
+// import LanguageDetector from 'i18next-browser-languagedetector';
+// //
+// import enLocales from './en.json';
+// import deLocales from './de.json';
+// import frLocales from './fr.json';
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: { translations: enLocales },
-      de: { translations: deLocales },
-      fr: { translations: frLocales }
-    },
-    lng: localStorage.getItem('i18nextLng') || 'en',
-    fallbackLng: 'en',
-    debug: false,
-    ns: ['translations'],
-    defaultNS: 'translations',
-    interpolation: {
-      escapeValue: false
-    }
-  });
+// // ----------------------------------------------------------------------
 
-export default i18n;
+// i18n
+//   .use(LanguageDetector)
+//   .use(initReactI18next)
+//   .init({
+//     resources: {
+//       en: { translations: enLocales },
+//       de: { translations: deLocales },
+//       fr: { translations: frLocales }
+//     },
+//     lng: localStorage.getItem('i18nextLng') || 'en',
+//     fallbackLng: 'en',
+//     debug: false,
+//     ns: ['translations'],
+//     defaultNS: 'translations',
+//     interpolation: {
+//       escapeValue: false
+//     }
+//   });
+
+// export default i18n;
