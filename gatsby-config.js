3b193f6600c244eef39d916b11cf2cb8b98ee240
Sean <>
2021-10-20 16:11:39 -0700

    maade addition to gatsby-config.js to include sample @chaec/gatbsy-source-chec publicKey from .env file...

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,6 +88,15 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: '@chec/gatsby-source-chec',
+      options: {
+        publicKey:
+          process.env.CHEC_PUBLIC_KEY ||
+          'pk_184625ed86f36703d7d233bcf6d519a4f9398f20048ec',
+        downloadImageAssets: true,
+      },
+    },
 
     {
       resolve: 'gatsby-source-google-spreadsheets',
