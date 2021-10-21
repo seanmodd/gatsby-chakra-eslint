7f33f43cc881742226cef3de8e19a587345c839a
Sean <>
2021-10-20 18:39:49 -0700

    added gatsby-transformer-remark to gatsby-config.js for any markdown files...

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-material-ui`,
     `gatsby-plugin-sitemap`,
+    `gatsby-transformer-remark`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-plugin-robots-txt`,
