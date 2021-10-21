9567dac47c23e6dc05dd1013eefb1a22a23ddd39
Sean <>
2021-10-21 00:48:52 -0700

    commented out the newproducts prop from ShopProductList.js

--- src/components/_MODERN/minimalComponents/_dashboard/e-commerce/shop/ShopProductList.js
+++ src/components/_MODERN/minimalComponents/_dashboard/e-commerce/shop/ShopProductList.js
@@ -2,21 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 // material
 import { Skeleton, Grid } from '@mui/material'
-import { products as newproducts } from '../../../../_apis_/products'
+// import { products as newproducts } from '../../../../_apis_/products'
 import ShopProductCard from './ShopProductCard'
 
 // ----------------------------------------------------------------------
 
+const SkeletonLoad = (
+  <>
+    {[...Array(12)].map((_, index) => (
+      <Grid item xs={12} sm={6} md={3} key={index}>
+        <Skeleton
+          variant="rectangular"
+          width="100%"
+          sx={{ paddingTop: '115%', borderRadius: 2 }}
+        />
+      </Grid>
+    ))}
+  </>
+)
 
-// ProductList.propTypes = {
-//   products: PropTypes.array.isRequired,
-//   isLoad: PropTypes.bool,
-// }
+ProductList.propTypes = {
+  products: PropTypes.array.isRequired,
+  isLoad: PropTypes.bool,
+}
 
-// export default function ProductList({ products, isLoad, ...other }) {
 export default function ProductList({ products, isLoad, ...other }) {
-  console.log('THIS IS PROP OF NEWPRODUCTS: ', newproducts);
-  console.log('THIS IS PROP OF PRODUCTS: ', products);
+  // console.log('THIS IS PROP OF NEWPRODUCTS: ', newproducts)
+  console.log('THIS IS PROP OF PRODUCTS: ', products)
   return (
     <Grid container spacing={3} {...other}>
       {products.map(product => (
@@ -26,7 +38,7 @@ export default function ProductList({ products, isLoad, ...other }) {
         </Grid>
       ))}
 
-      {/* {isLoad && SkeletonLoad} */}
+      {isLoad && SkeletonLoad}
     </Grid>
   )
 }
